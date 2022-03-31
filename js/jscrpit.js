1615581f63fefda150efa7c4eaeecf4faf599c1b
Student <filip.kaselj@vuv.hr>
2022-03-31 10:45:01 +0200

    zad3

--- js/jscrpit.js
+++ js/jscrpit.js
@@ -1,8 +1,14 @@
+var i=1;
 function Gumb()
 {
 	var ime=$("#1").val();
 	var sem=$("#2").val();
 	var ects=$("#3").val();
 
-	$("#tablica").append('<tr><th>'+ime+'</th><th>'+sem+'</th><th>'+ects+'</th>tr>');
+	$("#tablica").append('<tr><th>'+ime+'</th><th>'+sem+'</th><th>'+ects+'</th><th><button id="obrisi" type="button">Obrisi</button></th></tr>');
+	i++;
 }
+
+$("table").on('click', '#obrisi', function() {
+  $(this).closest('tr').remove();
+})
