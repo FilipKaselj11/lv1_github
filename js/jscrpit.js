fcb93b348fb5db3e1e9f9f78729ec8a38beb5a36
Student <filip.kaselj@vuv.hr>
2022-03-31 09:58:20 +0200

    zad2

---
+++ js/jscrpit.js
@@ -0,0 +1,8 @@
+function Gumb()
+{
+	var ime=$('#1').Val();
+	var sem=($'#2').Val();
+	var ects=($'#3').Val();
+
+	('#tablica').append('<tr><th>'+ime+'</th><th>'+semestar+'</th><th>'+ests+'</th>tr>');
+}
