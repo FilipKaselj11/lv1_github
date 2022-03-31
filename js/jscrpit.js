c44b17498b48e75fbc2e3b79954c590b45e90520
Student <filip.kaselj@vuv.hr>
2022-03-31 11:00:57 +0200

    zad4

--- js/jscrpit.js
+++ js/jscrpit.js
@@ -5,10 +5,23 @@ function Gumb()
 	var sem=$("#2").val();
 	var ects=$("#3").val();
 
-	$("#tablica").append('<tr><th>'+ime+'</th><th>'+sem+'</th><th>'+ects+'</th><th><button id="obrisi" type="button">Obrisi</button></th></tr>');
+	$("#tablica").append('<tr><th>'+ime+'</th><th>'+sem+'</th><th>'+ects+'</th><th><button id="obrisi" type="button">Obrisi</button></th>'+
+		'<th><input id="mjenjaj" type="checkbox"></th>'+'</tr>');
 	i++;
 }
 
 $("table").on('click', '#obrisi', function() {
   $(this).closest('tr').remove();
+})
+
+$("table").on('change', '#mjenjaj', function() {
+	if($(this).closest('tr').css("background-color")=="Red")
+	{
+		$(this).closest('tr').css("background-color","blue");
+	}
+	else 
+	{
+		$(this).closest('tr').css("background-color","red");
+	}
+
 })
